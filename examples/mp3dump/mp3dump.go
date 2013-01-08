5bba411927666e769ae3a0f36c356de2696cfb35
Robert Lowry <bobertlo@gmail.com>
2013-01-07 22:08:07 -0600

    small cleanup to mp3dump example and README

--- examples/mp3dump/mp3dump.go
+++ examples/mp3dump/mp3dump.go
@@ -14,42 +14,42 @@ func main() {
 	}
 
 	// create mpg123 decoder instance
-	dec, err := mpg123.NewMpg123("")
+	decoder, err := mpg123.NewMpg123("")
 	if err != nil {
 		panic("could not initialize mpg123")
 	}
 
 	// open a file with decoder
-	err = dec.Open(os.Args[1])
+	err = decoder.Open(os.Args[1])
 	if err != nil {
 		panic("error opening mp3 file")
 	}
+	defer decoder.Close()
 
-	// get encoding information
-	rate, chans, _ := dec.GetFormat()
+	// get audio format information
+	rate, chans, _ := decoder.GetFormat()
 	fmt.Fprintln(os.Stderr, "Encoding: Signed 16bit")
 	fmt.Fprintln(os.Stderr, "Sample Rate:", rate)
 	fmt.Fprintln(os.Stderr, "Channels:", chans)
 
 	// make sure output format does not change
-	dec.FormatNone()
-	dec.Format(rate, chans, mpg123.ENC_SIGNED_16)
+	decoder.FormatNone()
+	decoder.Format(rate, chans, mpg123.ENC_SIGNED_16)
 
 	// open output file
 	o, err := os.Create(os.Args[2])
 	if err != nil {
 		panic("error opening output file")
 	}
+	defer o.Close()
 
-	// decode mp3 file and dump output to a file
+	// decode mp3 file and dump output
 	buf := make([]byte, 2048*16)
 	for i := 0; i < 1024; i++ {
-		len, err := dec.Read(buf)
+		len, err := decoder.Read(buf)
 		o.Write(buf[0:len])
 		if err != nil {
 			break
 		}
 	}
-
-	o.Close()
 }
